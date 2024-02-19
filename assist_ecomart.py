33abe98732768bd2f2d2cafbd2c861380e4cc3e5
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-02-18 23:14:45 -0300

    first commit

---
+++ assist_ecomart.py
@@ -0,0 +1,33 @@
+from openai import *
+from dotenv import *
+import os
+from time import *
+from helpers import *
+from selecpersona import *
+
+load_dotenv()
+
+cliente =OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-3.5-turbo"
+contexto = carrega("dados\dados_ecomart.txt")
+
+def criar_thread():
+    return cliente.beta.threads.create()
+
+def criar_assistente():
+    assistente = cliente.beta.assistants.create(
+        name = "Atendente Ecomart",
+        instructions =  f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce.
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Além disso, adote a persona abaixo para responder o cliente.
+            
+            ##Contexto
+            {contexto}
+            
+            ## Persona
+            {personas["neutro"]}
+            """,
+        model = modelo
+    )
+    return assistente
