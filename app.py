ca18162f609c00e59434140c52c00422b3e3a272
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-02-21 12:10:12 -0300

    final version gpt3.5-turbo

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import *
-from openai import *
-from dotenv import *
+from flask import Flask,render_template, request, Response
+from openai import OpenAI
+from dotenv import load_dotenv
 import os
-from time import *
+from time import sleep
 from helpers import *
 from selecpersona import *
 from selectdoc import *
@@ -22,46 +22,45 @@ thread = criar_thread()
 def bot(prompt):
     maximo_tentativas = 1
     repeticao = 0
-    
+
     while True:
         try:
             cliente.beta.threads.messages.create(
-                thread_id= thread.id,
+                thread_id=thread.id, 
                 role = "user",
-                content = prompt
+                content =  prompt
             )
-            
+
             run = cliente.beta.threads.runs.create(
-                thread_id= thread.id,
-                assistant_id= assistente.id
+                thread_id=thread.id,
+                assistant_id=assistente.id
             )
-            
+
             while run.status !="completed":
                 run = cliente.beta.threads.runs.retrieve(
-                    thread_id= thread.id,
-                    run_id = run.id
-                )
+                    thread_id=thread.id,
+                    run_id=run.id
+            )
             
             historico = list(cliente.beta.threads.messages.list(thread_id=thread.id).data)
             resposta = historico[0]
-            return resposta            
-            
+            return resposta
+
         except Exception as erro:
-            repeticao += 1
-            if repeticao >= maximo_tentativas:
-                return "Erro no GPT: %s" % erro
-            print("Erro de comunicação com OpenAI", erro)
-            sleep(1)
-    
+                repeticao += 1
+                if repeticao >= maximo_tentativas:
+                        return "Erro no GPT: %s" % erro
+                print('Erro de comunicação com OpenAI:', erro)
+                sleep(1)
+            
 
-@app.route("/chat", methods =["POST"])
 
+@app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
     texto_resposta = resposta.content[0].text.value
     return texto_resposta
-    
 
 @app.route("/")
 def home():
--- assist_ecomart.py
+++ assist_ecomart.py
@@ -1,33 +1,34 @@
-from openai import *
-from dotenv import *
+from openai import OpenAI
+from dotenv import load_dotenv
 import os
-from time import *
+from time import sleep
 from helpers import *
 from selecpersona import *
 
 load_dotenv()
 
-cliente =OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-3.5-turbo"
-contexto = carrega("dados\dados_ecomart.txt")
+contexto = carrega("dados/ecomart.txt")
 
 def criar_thread():
     return cliente.beta.threads.create()
 
 def criar_assistente():
     assistente = cliente.beta.assistants.create(
-        name = "Atendente Ecomart",
-        instructions =  f"""
-            Você é um chatbot de atendimento a clientes de um e-commerce.
-            Você não deve responder perguntas que não sejam dados do ecommerce informado!
-            Além disso, adote a persona abaixo para responder o cliente.
-            
-            ##Contexto
-            {contexto}
-            
-            ## Persona
-            {personas["neutro"]}
-            """,
+        name="Atendente EcoMart",
+        instructions = f"""
+                Você é um chatbot de atendimento a clientes de um e-commerce. 
+                Você não deve responder perguntas que não sejam dados do ecommerce informado!
+                Além disso, adote a persona abaixo para respondero ao cliente.
+                
+                ## Contexto
+                {contexto}
+
+                ## Persona
+                {personas["neutro"]}
+                """,
         model = modelo
     )
     return assistente
+
